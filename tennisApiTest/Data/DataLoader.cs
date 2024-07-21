448e2ed46ff31179d31dfc8dda20bbe5c05efad6
abdessamad <abdessamad.aissaoui1@gmail.com>
2024-07-21 23:26:17 +0200

    Création du DataLoader pour charger les données & declaration des méthodes de l'interface IPlayerRepository

---
+++ tennisApiTest/Data/DataLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using tennisApiTest.Models;
+
+namespace tennisApiTest.Data
+{
+    public class DataLoader
+    {
+        public static List<Player> LoadPlayers(string path)
+        {
+            try
+            {
+                var json = System.IO.File.ReadAllText(path);
+                var playersData = JsonSerializer.Deserialize<PlayersList>(json);
+                return playersData?.players;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Class : DataLoader , Method : LoadPlayers , Erreur : Erreur lors du chargement des données des joueurs : {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ tennisApiTest/Repositories/IPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using tennisApiTest.Models;
+
+namespace tennisApiTest.Repositories
+{
+    public interface IPlayerRepository
+    {
+        // retourne la liste des joueurs
+        IEnumerable<Player> GetAllPlayers();
+
+
+        // retourne les informations d’un joueur grâce à son ID.
+        Player GetPlayerById(int id);
+    }
+}
