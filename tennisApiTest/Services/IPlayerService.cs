a45dd3d960e7ab3c9b9f953579a277b6a306c620
abdessamad <abdessamad.aissaoui1@gmail.com>
2024-07-21 23:31:10 +0200

    Création de l'interface IPlayerService

---
+++ tennisApiTest/Services/IPlayerService.cs
@@ -0,0 +1,16 @@
+﻿using tennisApiTest.Models;
+
+namespace tennisApiTest.Services
+{
+    public interface IPlayerService
+    {
+        // retourne les joueurs et trier du meilleur au moins bon.
+        IEnumerable<Player> GetSortedPlayers();
+
+        //retourner les informations d’un joueur grâce à son ID.
+        Player GetPlayerById(int id);
+
+        // retourne les statistiques
+        Statistics GetStatistics();
+    }
+}
